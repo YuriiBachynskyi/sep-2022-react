9aa21cb7e81dd7a02d31dfb2b4c4b5cdbd5e9f4c
YuriiBachynskyi <bachinskii911@gmail.com>
2023-01-22 16:56:56 +0200

    Add App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 const App = () => {
     return (
         <div>
-            AppLesson1
+            <div>
+                <div>name Homer</div>
+                <div>surname Simpson</div>
+                <img src="https://i.discogs.com/J4bH_-A4UcQHFSUBDyyqXbTzr7XWM8S0NfNoYgwXAiI/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE0MDAz/MTctMTMzNTcxNzQ3/Ni5wbmc.jpeg" alt="homer"/>
+            </div>
+
+            <div>
+                <div>name Marge</div>
+                <div>surname Simpson</div>
+                <img src="https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png" alt="Marge"/>
+            </div>
+
         </div>
     );
 };
